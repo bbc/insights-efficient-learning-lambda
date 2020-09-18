d84711c5745d1af64564e4d27e75ec1d46332f04
karthikeyan-yegappan <karthikeyan.yegappan@bbc.co.uk>
2020-09-18 09:41:09 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='efficient-learning-lambda',
-    version='1.3.0',
+    version='1.4.0',
     license='',
     description='Python Lambda which fetches Questions for Quizzes and Evaluates Results.',
     packages=find_packages('src'),
