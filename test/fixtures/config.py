1fc328b637bdc6e2d2e119f61003fd2491fcc1f3
duygu kokcu <duygu.kokcu@bbc.co.uk>
2020-09-28 14:46:43 +0100

    Test added for getting study_guide_per_topic_id  and topic_id_per_study_guide_id from config folder

---
+++ test/fixtures/config.py
@@ -0,0 +1,14 @@
+STUDY_GUIDES_IDS_PER_TOPIC_ID = {
+    "z2s8v9q": ["zc7k2nb", "z84jtv4", "zs8y4qt"],
+    "z9236yc": ["zt8t3k7", "zxr7ng8", "z3tgw6f", "z8fkmsg"], 
+    "zgws7p3": ["zcjy97h", "zg8nrwx"]
+}
+
+TOPIC_ID_PER_STUDY_GUIDE_ID = {
+    "zc7k2nb": "z2s8v9q",
+    "z84jtv4": "z2s8v9q",
+    "zs8y4qt": "z2s8v9q",
+    "zt8t3k7": "z9236yc",
+    "zxr7ng8": "z9236yc",
+    "z3tgw6f": "z9236yc"
+}
