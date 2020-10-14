01a6fc260b4185cc13f71e594572cc75de576c54
Lucia Mormocea <lucia.mormocea@bbc.co.uk>
2020-10-14 09:08:56 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='efficient-learning-lambda',
-    version='1.6.1',
+    version='1.6.2',
     license='',
     description='Python Lambda which fetches Questions for Quizzes and Evaluates Results.',
     packages=find_packages('src'),
