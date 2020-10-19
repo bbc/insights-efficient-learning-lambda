93bd4892ebdfedd4712c306fa8648fada8bc605e
drmattcrooks <matt.crooks@bbc.co.uk>
2020-10-19 17:03:40 +0100

    refactored to match flowchart structure

--- src/algorithm.py
+++ src/algorithm.py
@@ -29,38 +29,52 @@ def choose_initial_question(topic_id_for_study_guide_id, study_guide_id_list):
 
 
 def choose_next_question(topic_id_for_study_guide_id, study_guide_id_list, confidence_intervals_list, question_id_list):
+    probabilities_list = __convert_confidence_interval_into_probability(
+        confidence_intervals_list)
+
     study_guide_id = __choose_next_study_guide_id(
-        study_guide_id_list, confidence_intervals_list)
+        study_guide_id_list, probabilities_list)
 
     filtered_question_id_list = client.select_and_filter_question_ids(
         study_guide_id, question_id_list)
 
     if not filtered_question_id_list:
-        index = study_guide_id_list.index(study_guide_id)
+        filtered_study_guide_id_list, filtered_confidence_intervals_list = __ignore_completed_guide(
+            study_guide_id, study_guide_id_list, confidence_intervals_list)
 
-        filtered_study_guide_id_list = list(filter(
-            lambda x: x != study_guide_id, study_guide_id_list))
-
-        filtered_confidence_intervals_list = list(filter(
-            lambda x: confidence_intervals_list.index(x) != index, confidence_intervals_list))
-
-        return choose_next_question(topic_id_for_study_guide_id, filtered_study_guide_id_list, filtered_confidence_intervals_list, question_id_list)
+        return choose_next_question(topic_id_for_study_guide_id, filtered_study_guide_id_list,
+                                    filtered_confidence_intervals_list, question_id_list)
 
     question_id = random.choice(filtered_question_id_list)['id']
-
-    question = client.select_question_by_id(question_id, study_guide_id)
-
-    question.update({
-        "studyGuideId": study_guide_id,
-        "topicId": topic_id_for_study_guide_id[study_guide_id]
-    })
+    question = __get_question_text(
+        question_id, study_guide_id, topic_id_for_study_guide_id)
 
     return question
 
 
-def __choose_next_study_guide_id(study_guide_id_list, confidence_intervals_list):
-    probabilities_list = __convert_confidence_interval_into_probability(
-        confidence_intervals_list)
+def __get_question_text(question_id, study_guide_id, topic_id_for_study_guide_id):
+
+    question = client.select_question_by_id(question_id, study_guide_id)
+    question.update({
+        "studyGuideId": study_guide_id,
+        "topicId": topic_id_for_study_guide_id[study_guide_id]
+    })
+    return question
+
+
+def __ignore_completed_guide(study_guide_id, study_guide_id_list, confidence_intervals_list):
+    index = study_guide_id_list.index(study_guide_id)
+
+    filtered_study_guide_id_list = list(filter(
+        lambda x: x != study_guide_id, study_guide_id_list))
+
+    filtered_confidence_intervals_list = list(filter(
+        lambda x: confidence_intervals_list.index(x) != index, confidence_intervals_list))
+
+    return filtered_study_guide_id_list, filtered_confidence_intervals_list
+
+
+def __choose_next_study_guide_id(study_guide_id_list, probabilities_list):
 
     return np.random.choice(a=study_guide_id_list, p=probabilities_list)
 
@@ -76,11 +90,11 @@ def __normalise_list(list_):
     return [element / sum(list_) for element in list_]
 
 
-def calculate_weighted_score_and_attempts(
+def get_study_guide_weighting(
         study_guide_score, study_guide_attempts, topic_score, topic_attempts):
-    average_study_guide_mastery = __calculate_beta_distribution_mean(
+    average_study_guide_mastery = __(
         study_guide_score, study_guide_attempts)
-    average_topic_mastery = __calculate_beta_distribution_mean(
+    average_topic_mastery = __(
         topic_score, topic_attempts)
 
     study_guide_weighting = __calculate_thompson_sampling(
@@ -90,28 +104,10 @@ def calculate_weighted_score_and_attempts(
     if average_study_guide_mastery < average_topic_mastery:
         study_guide_weighting = (1 - study_guide_weighting)
 
-    weighted_score = __calculate_weighted_value(
-        study_guide_weighting, study_guide_score, topic_score)
-
-    weighted_attempts = __calculate_weighted_value(
-        study_guide_weighting, study_guide_attempts, topic_attempts)
-
-    return weighted_score, weighted_attempts
+    return study_guide_weighting
 
 
-def calculate_mastery_and_confidence(
-        weighted_score, weighted_attempts
-):
-    mastery = __calculate_beta_distribution_mean(
-        weighted_score, weighted_attempts)
-
-    confidence_interval = __calculate_confidence_interval(
-        weighted_score, weighted_attempts)
-
-    return mastery, confidence_interval
-
-
-def __calculate_beta_distribution_mean(score, attempts):
+def __(score, attempts):
     return (score + 1) / ((score + 1) + (attempts + 1 - score))
 
 
@@ -139,7 +135,7 @@ def __calculate_thompson_sampling(study_guide_score, study_guide_attempts,
     return np.trapz(y=trapezium_heights, x=trapezium_edge_points)
 
 
-def __calculate_weighted_value(weighting, study_guide_value, topic_value):
+def calculate_weighted_value(weighting, study_guide_value, topic_value):
     return weighting * study_guide_value \
         + (1 - weighting) * topic_value
 
@@ -162,7 +158,7 @@ def __95th_percentile_equation(mastery, score, attempts):
     return beta.cdf(mastery, 1 + score, 1 + attempts - score) - 0.95
 
 
-def __calculate_confidence_interval(score, attempts):
+def calculate_confidence_interval(score, attempts):
     _95th_percentile = __calculate_95th_percentile(score, attempts)
     _5th_percentile = __calculate_5th_percentile(score, attempts)
     return _95th_percentile - _5th_percentile
--- src/index.py
+++ src/index.py
@@ -19,14 +19,106 @@ def handler(event, context):
     topic_id_for_study_guide_id = helper.get_topic_id(
         study_guide_id_list)
 
-    question_id_list = []
-
     if not questions:
         next_question = {
             'nextQuestion': algorithm.choose_initial_question(topic_id_for_study_guide_id, study_guide_id_list)
         }
 
         return __build_response(200, next_question)
+
+    question_id_list = __get_question_ids_list(questions)
+
+    study_guide_score_and_attempts, topic_score_and_attempts = __get_topic_and_guide_score_and_attempts(
+        topic_id_list, study_guide_id_list, questions)
+
+    weighted_score_and_attempts = __calculate_weighted_score_and_attempts(
+        study_guide_score_and_attempts, topic_score_and_attempts,
+        topic_id_for_study_guide_id)
+
+    if return_results:
+        response = {
+            'results': []
+        }
+        for study_guide_id, guide_weighted_score_and_attempts in weighted_score_and_attempts.items():
+
+            weighted_score, weighted_attempts = __get_weighted_score_and_attempts(
+                guide_weighted_score_and_attempts)
+
+            mastery = algorithm.calculate_beta_distribution_mean(
+                weighted_score, weighted_attempts)
+
+            mastery_band, band_confidence = algorithm.calculate_mastery_band_and_confidence(
+                mastery, weighted_score, weighted_attempts)
+
+            response['results'].append({
+                'studyGuideId': study_guide_id,
+                'topicId': topic_id_for_study_guide_id[study_guide_id],
+                'band': mastery_band,
+                'masteryScore': mastery * 100,
+                'confidenceScore': band_confidence * 100
+            })
+    else:
+        confidence_intervals_list = __calculate_confidence_intervals_list(
+            study_guide_id_list, weighted_score_and_attempts)
+
+        response = {
+            'nextQuestion': algorithm.choose_next_question(
+                topic_id_for_study_guide_id, study_guide_id_list,
+                confidence_intervals_list, question_id_list)
+        }
+
+    return __build_response(200, response)
+
+
+def __get_weighted_score_and_attempts(weighted_score_and_attempts):
+    return weighted_score_and_attempts['score'], weighted_score_and_attempts['attempts']
+
+
+def __calculate_weighted_score_and_attempts(
+        study_guide_score_and_attempts, topic_score_and_attempts,
+        topic_id_for_study_guide_id):
+
+    study_guide_id_list = list(study_guide_score_and_attempts.keys())
+    weighted_score_and_attempts = __initialise_score_and_attempts(study_guide_id_list)
+
+    for study_guide_id, guide_weighted_score_and_attempts in weighted_score_and_attempts.items():
+        topic_id = topic_id_for_study_guide_id[study_guide_id]
+        topic_score = topic_score_and_attempts[topic_id]['score']
+        topic_attempts = topic_score_and_attempts[topic_id]['attempts']
+
+        study_guide_score = study_guide_score_and_attempts[study_guide_id]['score']
+        study_guide_attempts = study_guide_score_and_attempts[study_guide_id]['attempts']
+
+        study_guide_weighting = algorithm.get_study_guide_weighting(
+            study_guide_score, study_guide_attempts, topic_score, topic_attempts)
+
+        guide_weighted_score_and_attempts['score'] = algorithm.calculate_weighted_value(
+            study_guide_weighting, study_guide_score, topic_score)
+
+        guide_weighted_score_and_attempts['attempts'] = algorithm.calculate_weighted_value(
+            study_guide_weighting, study_guide_attempts, topic_attempts)
+
+    return weighted_score_and_attempts
+
+
+def __calculate_confidence_intervals_list(study_guide_id_list,
+                                          weighted_score_and_attempts):
+    confidence_intervals_list = []
+    for study_guide_id in study_guide_id_list:
+        weighted_score = weighted_score_and_attempts[study_guide_id]['score']
+        weighted_attempts = weighted_score_and_attempts[study_guide_id]['attempts']
+
+        confidence_intervals_list.append(algorithm.calculate_confidence_interval(
+            weighted_score, weighted_attempts))
+    return confidence_intervals_list
+
+
+def __get_question_ids_list(questions):
+    return [question['id'] for question in questions]
+
+
+def __get_topic_and_guide_score_and_attempts(topic_id_list, study_guide_id_list,
+                                             questions):
 
     topic_score_and_attempts = __initialise_score_and_attempts(topic_id_list)
     study_guide_score_and_attempts = __initialise_score_and_attempts(
@@ -36,48 +128,8 @@ def handler(event, context):
         __update_topic_score_and_attempts(topic_score_and_attempts, question)
         __update_study_guide_score_and_attempts(
             study_guide_score_and_attempts, question)
-        question_id_list.append(question['id'])
 
-    results_list = []
-    confidence_intervals_list = []
-
-    for study_guide_id in study_guide_id_list:
-        topic_id = topic_id_for_study_guide_id[study_guide_id]
-        topic_score = topic_score_and_attempts[topic_id]['score']
-        topic_attempts = topic_score_and_attempts[topic_id]['attempts']
-
-        study_guide_score = study_guide_score_and_attempts[study_guide_id]['score']
-        study_guide_attempts = study_guide_score_and_attempts[study_guide_id]['attempts']
-
-        weighted_score, weighted_attempts = algorithm.calculate_weighted_score_and_attempts(
-            study_guide_score, study_guide_attempts, topic_score, topic_attempts)
-
-        mastery, confidence_interval = algorithm.calculate_mastery_and_confidence(
-            weighted_score, weighted_attempts)
-
-        confidence_intervals_list.append(confidence_interval)
-
-        mastery_band, band_confidence = algorithm.calculate_mastery_band_and_confidence(
-            mastery, weighted_score, weighted_attempts)
-
-        results_list.append({
-            'studyGuideId': study_guide_id,
-            'topicId': topic_id,
-            'band': mastery_band,
-            'masteryScore': mastery * 100,
-            'confidenceScore': band_confidence * 100
-        })
-
-    next_question = {
-        'nextQuestion': algorithm.choose_next_question(topic_id_for_study_guide_id, study_guide_id_list, confidence_intervals_list, question_id_list)
-    }
-
-    if return_results:
-        next_question.update({
-            'results': results_list
-        })
-
-    return __build_response(200, next_question)
+    return study_guide_score_and_attempts, topic_score_and_attempts
 
 
 def __initialise_score_and_attempts(list_):
