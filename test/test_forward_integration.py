17eebf9ccae64cb42063bd92d96f31eadf4fa37d
drmattcrooks <matt.crooks@bbc.co.uk>
2020-11-19 15:52:35 +0000

    forward integration

---
+++ test/test_forward_integration.py
@@ -0,0 +1,20 @@
+import pytest
+import algorithm
+
+
+@pytest.mark.fint_calculate_study_guide_weighting
+def test_calculate_study_guide_weighting_passes_into_calculate_weighted_value():
+    study_guide_score = 0.
+    study_guide_attempts = 100.
+    topic_score = 100.
+    topic_attempts = 100.
+
+    study_guide_weighting = algorithm._calculate_study_guide_weighting(
+        study_guide_score, study_guide_attempts, topic_score, topic_attempts)
+
+    try:
+        weighted_score = algorithm._calculate_weighted_value(
+            study_guide_weighting, study_guide_score, topic_score)
+        assert True
+    except:
+        assert False
